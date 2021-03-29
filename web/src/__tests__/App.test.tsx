b02ccbb6b5a5c97b4d8cc4466f8cb800b7babdf0
Meepanya <Meepanya@users.noreply.github.com>
2021-03-29 02:02:33 +0200

    II. configured jest&react-testing-library for ts.

---
+++ web/src/__tests__/App.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import App from "../App";
+
+describe("<App />", () => {
+    test("renders without crashing", async () => {
+        
+    });
+});
--- web/src/components/Cart.tsx
+++ web/src/components/Cart.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { createContext } from "react"
 
 import Popup from "./Popup";
 
